e27c3088eec9ac9cf37a80b279193f3c4ee8a742
drewvrieze <vriezedrew@gmail.com>
2026-04-13 04:20:41 +0000

    hw

---
+++ Assignments/Section-13.10-reversefile.py
@@ -0,0 +1,36 @@
+#----------------------------
+# Name: Section 13 reversefile
+# Purpose: learn how to open and use files 
+#
+# Author: DrewV
+#
+# Created: 04/12/2026
+#----------------------------
+# I thought this was funny so i kept it
+"""
+def reverselines(file):
+    old = open(file, "r")
+    new = open(file + ".reversed", "w")
+    while True:
+        buf = old.readline()
+        if len(buf) == 0:
+            break
+        new.write(old[::-1])
+
+
+    old.close()
+    new.close()
+
+reverselines("sup")
+"""
+def reverselines(file):
+    old = open(file, "r")
+    new = open(file + ".reversed", "w")
+        buf = old.readlines()
+        for line in reversed(buf):
+            new.write(line)
+
+    old.close()
+    new.close()
+
+reverselines("sup")
